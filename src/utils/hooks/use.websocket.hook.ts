fb77b58c366013dbf942daf25c0657902a612d0c
Nils Kortmann <kortmannnils@gmail.com>
2024-01-01 23:13:50 +0100

    fix useWebSocket hook

--- src/utils/hooks/use.websocket.hook.ts
+++ src/utils/hooks/use.websocket.hook.ts
@@ -1,20 +1,26 @@
 import { useEffect } from 'react';
 import WebSocketClient from '../websocket/WebSocketClient';
 import WebSocketClientWrapper from '../websocket/WebSocketClientWrapper';
+import { useAppSelector } from '../../store/hooks';
+import { selectLoggedIn } from '../../store/features/authentication.slice';
 
 export let globalWebSocketClient: WebSocketClient;
 
 const useWebSocket = (
   callback: (webSocketClientWrapper: WebSocketClientWrapper) => void | (() => void)
 ) => {
+  const isLoggedIn = useAppSelector(selectLoggedIn);
+
   useEffect(() => {
-    if (!globalWebSocketClient) {
-      globalWebSocketClient = new WebSocketClient();
+    if (isLoggedIn) {
+      if (!globalWebSocketClient) {
+        globalWebSocketClient = new WebSocketClient();
+      }
+
+      const webSocketClientWrapper = new WebSocketClientWrapper(globalWebSocketClient, callback);
+
+      return () => webSocketClientWrapper.subscriptions().unsubscribeAll();
     }
-
-    const webSocketClientWrapper = new WebSocketClientWrapper(globalWebSocketClient, callback);
-
-    return () => webSocketClientWrapper.subscriptions().unsubscribeAll();
   }, []);
 };
 
