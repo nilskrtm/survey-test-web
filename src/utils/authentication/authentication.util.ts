621314a9603c5a4cd8d606391a47170ba5e08c69
Nils Kortmann <kortmannnils@gmail.com>
2024-03-11 23:55:52 +0100

    small improvement

--- src/utils/authentication/authentication.util.ts
+++ src/utils/authentication/authentication.util.ts
@@ -1,7 +1,12 @@
 import { JwtHeader, JwtPayload, jwtDecode } from 'jwt-decode';
 import { AuthenticationState } from '../../store/features/authentication.slice';
 
-export const parseTokenData = (token: string) => {
+type TokenData = {
+  header: JwtHeader;
+  payload: JwtPayload & AuthenticationState;
+};
+
+export const parseTokenData: (token: string) => TokenData = (token) => {
   const decodedHeader: JwtHeader = jwtDecode<JwtHeader>(token, { header: true });
   const decodedPayload: JwtPayload & AuthenticationState = jwtDecode<
     JwtPayload & AuthenticationState
