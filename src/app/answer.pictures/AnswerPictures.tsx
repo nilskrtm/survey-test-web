c1dcc461443c4ad6dde1163177114cee35b9b4bd
Nils Kortmann <kortmannnils@gmail.com>
2024-03-21 00:25:06 +0100

    small change

--- src/app/answer.pictures/AnswerPictures.tsx
+++ src/app/answer.pictures/AnswerPictures.tsx
@@ -5,9 +5,9 @@ const AnswerPictures: () => React.JSX.Element = () => {
   useDashboardTitle('Meine Bilder');
 
   return (
-    <div className="w-full h-full grid grid-cols-1 gap-12 p-6 overflow-y-scroll">
-      <div className="w-full flex flex-row items-center justify-between rounded-lg bg-white border border-gray-200 py-10 px-10">
-        <p>Bilder</p>
+    <div className="w-full grid auto-rows-min grid-cols-1 gap-4 p-6 overflow-y-scroll">
+      <div className="w-full flex flex-row items-center justify-between rounded-lg bg-white border border-gray-200 p-6">
+        <p>Bilder für Antworten</p>
       </div>
     </div>
   );
--- src/app/votings/Votings.tsx
+++ src/app/votings/Votings.tsx
@@ -5,8 +5,8 @@ const Votings: () => React.JSX.Element = () => {
   useDashboardTitle('Abstimmungen');
 
   return (
-    <div className="w-full h-full grid grid-cols-1 gap-12 p-6 overflow-y-scroll">
-      <div className="w-full flex flex-row items-center justify-between rounded-lg bg-white border border-gray-200 py-10 px-10">
+    <div className="w-full grid auto-rows-min grid-cols-1 gap-4 p-6 overflow-y-scroll">
+      <div className="w-full flex flex-row items-center justify-between rounded-lg bg-white border border-gray-200 p-6">
         <p>Abstimmungen</p>
       </div>
     </div>
