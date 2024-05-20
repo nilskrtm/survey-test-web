c0a368b712335a3a40910014ef31fed4ae742f43
Nils Kortmann <kortmannnils@gmail.com>
2024-05-20 16:41:36 +0200

    small improvement

--- src/utils/query/query.params.util.ts
+++ src/utils/query/query.params.util.ts
@@ -2,9 +2,9 @@ import { QuerySearchParams } from '../hooks/use.query.params.hook';
 
 type ParsedQuerySearchParams<V> = { [P in keyof V]: V[P] };
 
-const parseQuerySearchParams: <T>(queryParams: QuerySearchParams) => ParsedQuerySearchParams<T> = (
+const parseQuerySearchParams: <T extends QuerySearchParams>(
   queryParams: QuerySearchParams
-) => {
+) => ParsedQuerySearchParams<T> = (queryParams: QuerySearchParams) => {
   const result: { [key: string]: string | number } = {};
 
   for (const param in queryParams) {
