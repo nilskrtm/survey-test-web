3d6c8a510bc472e9b114ca2a42f4f3113ad7e9d0
Nils Kortmann <kortmannnils@gmail.com>
2024-05-20 15:26:50 +0200

    small improvement

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,16 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr(), eslint()],
   build: {
     target: 'esnext',
-    outDir: 'build'
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   },
   define: {
     'process.env': process.env
