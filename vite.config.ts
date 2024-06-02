86f99d5ce5cefd78cd29f7b882159e3ebcb5026f
Nils Kortmann <kortmannnils@gmail.com>
2024-06-02 16:23:53 +0200

    update caching policy

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,7 @@ export default defineConfig({
     outDir: 'build',
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
+        chunkFileNames: `[name].[hash].js`
       }
     }
   },
