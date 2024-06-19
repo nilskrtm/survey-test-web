93e8d47cb829918b2d9b98aa7660bc459a702edd
Nils Kortmann <kortmannnils@gmail.com>
2024-06-19 15:35:21 +0200

    update import-meta-env interface

--- src/import-meta-env.d.ts
+++ src/import-meta-env.d.ts
@@ -1,19 +1,19 @@
 interface ImportMetaEnv {
-  readonly HTML_TITLE?: string;
-  readonly HTML_DESCRIPTION?: string;
-  readonly IMPRINT_ADDRESS_LINE_1?: string;
-  readonly IMPRINT_ADDRESS_LINE_2?: string;
-  readonly IMPRINT_ADDRESS_LINE_3?: string;
-  readonly IMPRINT_ADDRESS_LINE_4?: string;
-  readonly IMPRINT_CONTACT_EMAIL?: string;
-  readonly IMPRINT_CONTACT_PHONE?: string;
-  readonly APP_DOWNLOAD_URL?: string;
-  readonly API_ENDPOINT?: string;
-  readonly API_TIMEOUT?: number;
-  readonly WS_ENDPOINT?: string;
-  readonly WS_RECONNECT_TIMEOUT?: number;
+  readonly HTML_TITLE: string;
+  readonly HTML_DESCRIPTION: string;
+  readonly IMPRINT_ADDRESS_LINE_1: string;
+  readonly IMPRINT_ADDRESS_LINE_2: string;
+  readonly IMPRINT_ADDRESS_LINE_3: string;
+  readonly IMPRINT_ADDRESS_LINE_4: string;
+  readonly IMPRINT_CONTACT_EMAIL: string;
+  readonly IMPRINT_CONTACT_PHONE: string;
+  readonly APP_DOWNLOAD_URL: string;
+  readonly API_ENDPOINT: string;
+  readonly API_TIMEOUT: number;
+  readonly WS_ENDPOINT: string;
+  readonly WS_RECONNECT_TIMEOUT: number;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: Partial<ImportMetaEnv>;
 }
